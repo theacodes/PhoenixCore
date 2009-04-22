7a399e0d7c94792acaf4b01fddeec68fc519e8d6
Jonathan Wayne Parrott <jjramone13@gmail.com>
2009-04-22 01:17:56 +0000

    - Commented sources for PhLight and PhLightManager.

--- libPhoenixGL/PhLight.cpp
+++ libPhoenixGL/PhLight.cpp
@@ -2,16 +2,33 @@
 
 using namespace phoenix;
 
-PhLight::PhLight(PhLightManager* l, PhTexture* t, PhVector2d p, PhColor c, PhVector2d s): lmgr(l), texture(t), position(p), color(c), scale(s)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Ctor
+	All we have to do is add the light.
+*/
+PhLight::PhLight(PhLightManager* l, PhTexture* t, PhVector2d p, PhColor c, PhVector2d s):
+	lmgr(l), texture(t), position(p), color(c), scale(s)
 {
     lmgr->addLight(this);
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Dtor
+	All we have to do is remove the light from the list.
+*/
 PhLight::~PhLight()
 {
     lmgr->removeLight(this);
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Draw function.
+	All we have to do here is draw the texture
+	just like we normally would with a node.
+*/
 void PhLight::draw()
 {
     lmgr->getSceneManager()->getRenderSystem()->drawTexture(texture,position-(texture->getSize()/2.0f),0.0f,0.0f,scale,color);
--- libPhoenixGL/PhLightManager.cpp
+++ libPhoenixGL/PhLightManager.cpp
@@ -2,22 +2,41 @@
 
 using namespace phoenix;
 
-PhLightManager::PhLightManager(PhSceneManager* s, PhVector2d ts) : smgr(s)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Ctor
+	Just make our render target
+*/
+PhLightManager::PhLightManager(PhSceneManager* s, PhVector2d ts) :
+	smgr(s)
 {
     PhTexture* target = new PhTexture(smgr->getRenderSystem()->getTextureManager(), (int)ts.getX(), (int)ts.getY());
     rtexture = new PhRenderTexture(smgr->getRenderSystem(), target);
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Dtor
+	Just delete all the lights.
+*/
 PhLightManager::~PhLightManager()
 {
     clearLights();
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Add light
+*/
 void PhLightManager::addLight(PhLight* l)
 {
     lights.push_back(l);
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Remove light
+*/
 void PhLightManager::removeLight(PhLight* l)
 {
     for (unsigned int i = 0; i < lights.size(); i++)
@@ -29,6 +48,11 @@ void PhLightManager::removeLight(PhLight* l)
     }
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Clear lights
+	Deletes every light in the list.
+*/
 void PhLightManager::clearLights()
 {
     for (unsigned int i = 0; i < lights.size(); i++)
@@ -40,15 +64,21 @@ void PhLightManager::clearLights()
     }
     lights.clear();
 }
-
+- Comment the source, at least a little.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Generate the light buffer.
+*/
 void PhLightManager::generateBuffer()
 {
+
+	//start rendering to our render texture
     rtexture->startRender();
 
+    //set the blend mode
     smgr->getRenderSystem()->setBlendMode(GL_SRC_ALPHA,GL_ONE);
 
-    //smgr->getView()->setGLView();
-
+	// iterate through the lights and draw them all.
     for (unsigned int i = 0; i < lights.size(); i++)
     {
         if (lights[i]!=NULL)
@@ -57,15 +87,22 @@ void PhLightManager::generateBuffer()
         }
     }
 
+    // restore the blend mode.
     smgr->getRenderSystem()->setDefaultBlendMode();
 
+    // End our rendering.
     rtexture->endRender();
 
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+	Render the buffer.
+*/
 void PhLightManager::renderBuffer( float depth )
 {
 
+	// This blend mode allows lights.
     smgr->getRenderSystem()->setBlendMode(GL_DST_COLOR, GL_ZERO);
 
     glMatrixMode(GL_MODELVIEW);
