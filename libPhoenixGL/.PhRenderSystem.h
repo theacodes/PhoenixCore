ac4c4511b5dc1a61b62c9e1a88daef94e2d43148
Jonathan Wayne Parrott <jjramone13@gmail.com>
2009-05-08 15:40:32 +0000

    Fixed a small copyright error, and updated the docs (again).

--- libPhoenixGL/.PhRenderSystem.h
+++ libPhoenixGL/.PhRenderSystem.h
@@ -40,7 +40,7 @@ THE SOFTWARE.
 
 //! The phoenix namespace.
 /*!
-Copyright (c) 2007, Jonathan Wayne Parrott, Denzel Morris
+Copyright (c) 2007, Jonathan Wayne Parrott.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
